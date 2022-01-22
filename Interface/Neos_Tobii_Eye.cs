516ddb3389b285f7ddf1f544369463a201b7c85e
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-01-22 14:10:31 -0600

    consolidated the files bc neos mod loader will only run mods with one class like this

--- Interface/Neos_Tobii_Eye.cs
+++ Interface/Neos_Tobii_Eye.cs
@@ -4,11 +4,10 @@ using FrooxEngine;
 using BaseX;
 using System;
 using Tobii.Research;
-using System.IO;
-using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
-using Neos_Tobii_Eye_Integration.Helpers;
+using System.IO;
+using System.Diagnostics;
 
 namespace Neos_Tobii_Eye_Integration
 {
@@ -19,7 +18,7 @@ namespace Neos_Tobii_Eye_Integration
 		// Possibly add calibration?
 
 		public static IEyeTracker eyeTracker;
-		public static bool usingTobiiScreen = false;
+		public static bool usingTobiiScreen = true;
 
 		public static bool leftIsValid;
 		public static float leftBlink;
@@ -37,7 +36,7 @@ namespace Neos_Tobii_Eye_Integration
 
 		public override string Name => "Neos-Tobii-Eye-Integration";
 		public override string Author => "dfgHiatus";
-		public override string Version => "alpha-1.0.3";
+		public override string Version => "alpha-1.0.4";
 		public override string Link => "https://github.com/dfgHiatus/Neos-Tobii-Eye-Tracker-Integration";
 
 		public override void OnEngineInit()
@@ -58,12 +57,12 @@ namespace Neos_Tobii_Eye_Integration
 					// IIRC this doesn't work for VR Devices
 					if (usingTobiiScreen)
                     {
-						eyeTracker.GazeDataReceived += TobiiScreen.EyeTracker_GazeDataReceived;
-						TobiiScreen.CallEyeTrackerManager(eyeTracker);
+						eyeTracker.GazeDataReceived += EyeTracker_GazeDataReceived;
+						CallEyeTrackerManager(eyeTracker);
 					}
 					else
                     {
-						eyeTracker.HMDGazeDataReceived += TobiiVR.EyeTracker_HMDGazeDataReceived;
+						eyeTracker.HMDGazeDataReceived += EyeTracker_HMDGazeDataReceived;
 					}
 					
 					eyeTracker.ConnectionLost += EyeTracker_ConnectionLost;
@@ -104,6 +103,131 @@ namespace Neos_Tobii_Eye_Integration
 			}
 		}
 
+		public static void CallEyeTrackerManager(IEyeTracker eyeTracker)
+		{
+			string etmStartupMode = "displayarea";
+			string etmBasePath = Path.GetFullPath(Path.Combine(Environment.GetEnvironmentVariable("LocalAppData"),
+																"TobiiProEyeTrackerManager"));
+			string appFolder = Directory.EnumerateDirectories(etmBasePath, "app*").FirstOrDefault();
+			string executablePath = Path.GetFullPath(Path.Combine(etmBasePath,
+																	appFolder,
+																	"TobiiProEyeTrackerManager.exe"));
+			string arguments = "--device-address=" + eyeTracker.Address + " --mode=" + etmStartupMode;
+			try
+			{
+				Process etmProcess = new Process();
+				// Redirect the output stream of the child process.
+				etmProcess.StartInfo.UseShellExecute = false;
+				etmProcess.StartInfo.RedirectStandardError = true;
+				etmProcess.StartInfo.RedirectStandardOutput = true;
+				etmProcess.StartInfo.FileName = executablePath;
+				etmProcess.StartInfo.Arguments = arguments;
+				etmProcess.Start();
+				string stdOutput = etmProcess.StandardOutput.ReadToEnd();
+
+				etmProcess.WaitForExit();
+				int exitCode = etmProcess.ExitCode;
+				if (exitCode == 0)
+				{
+					Debug("Eye Tracker Manager was called successfully");
+				}
+				else
+				{
+					Warn("Eye Tracker Manager call returned the error code: {0}", exitCode);
+					foreach (string line in stdOutput.Split(Environment.NewLine.ToCharArray()))
+					{
+						if (line.StartsWith("ETM Error:"))
+						{
+							Error(line);
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Error("An unexpected error occured when trying to initiallize the Tobii Eye Manager.");
+				Error(e.Message);
+			}
+		}
+
+		public static void EyeTracker_GazeDataReceived(object sender, GazeDataEventArgs e)
+		{
+			// TODO Check if both are valid post debugging
+
+			leftIsValid = e.LeftEye.GazeOrigin.Validity == Validity.Valid;
+			leftBlink = e.LeftEye.GazeOrigin.Validity == Validity.Valid ? 1f : 0f;
+			leftRawPupil = e.LeftEye.Pupil.PupilDiameter;
+
+			if (e.LeftEye.GazeOrigin.Validity == Validity.Valid && e.LeftEye.GazePoint.Validity == Validity.Valid)
+			{
+				leftRawPos = new float3(
+					e.LeftEye.GazeOrigin.PositionInUserCoordinates.X,
+					e.LeftEye.GazeOrigin.PositionInUserCoordinates.Y,
+					e.LeftEye.GazeOrigin.PositionInUserCoordinates.Z);
+				leftRawRot = new float3(
+					e.LeftEye.GazePoint.PositionInUserCoordinates.X,
+					e.LeftEye.GazePoint.PositionInUserCoordinates.Y,
+					e.LeftEye.GazePoint.PositionInUserCoordinates.Z);
+			}
+
+			rightIsValid = e.RightEye.GazeOrigin.Validity == Validity.Valid;
+			rightBlink = e.RightEye.GazeOrigin.Validity == Validity.Valid ? 1f : 0f;
+			rightRawPupil = e.RightEye.Pupil.PupilDiameter;
+
+			if (e.RightEye.GazeOrigin.Validity == Validity.Valid && e.RightEye.GazePoint.Validity == Validity.Valid)
+			{
+				rightRawPos = new float3(
+					e.RightEye.GazeOrigin.PositionInUserCoordinates.X,
+					e.RightEye.GazeOrigin.PositionInUserCoordinates.Y,
+					e.RightEye.GazeOrigin.PositionInUserCoordinates.Z);
+				rightRawRot = new float3(
+					e.RightEye.GazePoint.PositionInUserCoordinates.X,
+					e.RightEye.GazePoint.PositionInUserCoordinates.Y,
+					e.RightEye.GazePoint.PositionInUserCoordinates.Z);
+			}
+
+			timestamp = e.DeviceTimeStamp;
+
+		}
+
+		public static void EyeTracker_HMDGazeDataReceived(object sender, HMDGazeDataEventArgs e)
+		{
+			// TODO Check if both are valid post debugging
+			leftIsValid = e.LeftEye.GazeOrigin.Validity == Validity.Valid;
+			leftBlink = e.LeftEye.GazeOrigin.Validity == Validity.Valid ? 1f : 0f;
+			leftRawPupil = e.LeftEye.Pupil.PupilDiameter;
+
+			if (e.LeftEye.GazeOrigin.Validity == Validity.Valid && e.LeftEye.GazeDirection.Validity == Validity.Valid)
+			{
+				leftRawPos = new float3(
+					e.LeftEye.GazeOrigin.PositionInHMDCoordinates.X,
+					e.LeftEye.GazeOrigin.PositionInHMDCoordinates.Y,
+					e.LeftEye.GazeOrigin.PositionInHMDCoordinates.Z);
+				leftRawRot = new float3(
+					e.LeftEye.GazeDirection.UnitVector.X,
+					e.LeftEye.GazeDirection.UnitVector.Y,
+					e.LeftEye.GazeDirection.UnitVector.Z);
+			}
+
+			rightIsValid = e.RightEye.GazeOrigin.Validity == Validity.Valid;
+			rightBlink = e.RightEye.GazeOrigin.Validity == Validity.Valid ? 1f : 0f;
+			rightRawPupil = e.RightEye.Pupil.PupilDiameter;
+
+			if (e.RightEye.GazeOrigin.Validity == Validity.Valid && e.RightEye.GazeDirection.Validity == Validity.Valid)
+			{
+				rightRawPos = new float3(
+					e.RightEye.GazeOrigin.PositionInHMDCoordinates.X,
+					e.RightEye.GazeOrigin.PositionInHMDCoordinates.Y,
+					e.RightEye.GazeOrigin.PositionInHMDCoordinates.Z);
+				rightRawRot = new float3(
+					e.RightEye.GazeDirection.UnitVector.X,
+					e.RightEye.GazeDirection.UnitVector.Y,
+					e.RightEye.GazeDirection.UnitVector.Z);
+			}
+
+			timestamp = e.DeviceTimeStamp;
+		}
+
 		public static void EyeTracker_ConnectionLost(object sender, ConnectionLostEventArgs e)
         {
 			Debug("Connection was lost to the Tobii eye tracking service.");
@@ -123,11 +247,11 @@ namespace Neos_Tobii_Eye_Integration
 				{
 					if (usingTobiiScreen)
                     {
-						eyeTracker.GazeDataReceived -= TobiiScreen.EyeTracker_GazeDataReceived;
+						eyeTracker.GazeDataReceived -= EyeTracker_GazeDataReceived;
 					}
 					else
                     {
-						eyeTracker.HMDGazeDataReceived -= TobiiVR.EyeTracker_HMDGazeDataReceived;
+						eyeTracker.HMDGazeDataReceived -= EyeTracker_HMDGazeDataReceived;
 					}
 					eyeTracker.ConnectionLost -= EyeTracker_ConnectionLost;
 					eyeTracker.ConnectionRestored -= EyeTracker_ConnectionRestored;
